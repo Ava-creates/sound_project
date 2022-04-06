908c4be1407a27822b988db9c8d233ca357f52ba
Ava-creates <avanitiwari2002@gmail.com>
2022-04-06 19:41:12 +0400

    update

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("crow").onclick = function() {f1()}  ; 
+
+function f1()
+{
+    console.log("sdg");
+    window.scrollTo({
+        top:(window.innerHeight)*1 ,
+        
+        behavior: 'smooth'
+      });
+}
+
+
+
